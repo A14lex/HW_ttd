73fee9e477c6b5db587d9ec5b53ecc92180f72e3
Alex330790 <ajkul@mail.ru>
2024-08-02 22:10:49 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    @Test
+    public void testPhoneBookAdd(){
+        Integer i = null;
+        PhoneBook phoneBook = new PhoneBook();
+         i = phoneBook.add("noName", "123");
+        Assertions.assertTrue(i!=0, "Количество записей не изменилось после добавления нового имени");
+    }
+    public void testRepeatNumberAdd(){
+        String name1 = "Nonna";
+        String name2 = "Nonna";
+        PhoneBook phoneBook = new PhoneBook();
+        int countWithName1 = phoneBook.add(name1, "123");
+
+        int countWithName2 = phoneBook.add(name2, "123");
+        //Добавлены две записи с одинаковыми именами - кол-во записей должно быть 1
+        Assertions.assertTrue(countWithName2==1, "Два одинаковых имени не изменили кол-во записей должным образом (на 1 т.е.)");
+    }
 }
