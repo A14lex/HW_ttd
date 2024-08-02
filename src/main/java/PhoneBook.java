612f6eb12f6be882e47d516dd9f9fed5bf01357b
Alex330790 <ajkul@mail.ru>
2024-08-02 22:41:26 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
-    public int add(String name, String phoneNumber){
-        return 0;
+    //вывести или найти имя без полного перебора это наверно реализовать это в мапе
+    Map<String, String> map = new HashMap<>();
+
+    public int add(String name, String phoneNumber) {
+        if (!map.containsKey(name)) {
+            map.put(name, phoneNumber);
+        }
+        return map.size();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -3,12 +3,6 @@ import org.junit.jupiter.api.Test;
 
 public class PhoneBookTest {
     @Test
-    public void testPhoneBookAdd(){
-        Integer i = null;
-        PhoneBook phoneBook = new PhoneBook();
-         i = phoneBook.add("noName", "123");
-        Assertions.assertTrue(i!=0, "Количество записей не изменилось после добавления нового имени");
-    }
     public void testRepeatNumberAdd(){
         String name1 = "Nonna";
         String name2 = "Nonna";
@@ -19,4 +13,12 @@ public class PhoneBookTest {
         //Добавлены две записи с одинаковыми именами - кол-во записей должно быть 1
         Assertions.assertTrue(countWithName2==1, "Два одинаковых имени не изменили кол-во записей должным образом (на 1 т.е.)");
     }
+    @Test
+    public void testPhoneBookAdd(){
+        Integer i = null;
+        PhoneBook phoneBook = new PhoneBook();
+         i = phoneBook.add("noName", "123");
+        Assertions.assertTrue(i!=0, "Количество записей не изменилось после добавления нового имени");
+    }
+
 }
